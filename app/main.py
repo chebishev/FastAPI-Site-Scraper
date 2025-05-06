34951698db7f7b6e0cc06368105241e613686675
Atanas Chebishev <114015858+chebishev@users.noreply.github.com>
2025-05-06 09:44:26 +0300

    add description, documentation, tags

--- app/main.py
+++ app/main.py
@@ -2,12 +2,39 @@ from fastapi import FastAPI, Query
 import uvicorn
 from .scrapper import valid_links
 
+
+description = """
+    Get all hrefs from website in format https://example.com
+    """
+
+app = FastAPI(
+    title="Fast API SITE Scraper",
+    description=description,
+    summary="API that allow you to get all valid links from website in format https://example.com",
+    version="0.0.2",
+    contact={
+        "name": "Atanas Chebishev",
+        "url": "https://chebishev.github.io/",
+        "email": "atanas.chebishev@gmail.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "identifier": "MIT",
+    },
+)
+
+tags_metadata = [
+    {
+        "name": Scrapes links from a website",
+        "description": "This endpoint scrapes all the links from a given website (in format \"https://example.com/\") and returns them in a dictionary format where the key is the link address and the value is the link text.",
+    },
+]
+
 app = FastAPI()
 
+#Todo: add the tag to the endpoint
 @app.get(
-    "/valid-links", 
-    summary="Scrapes links from a website",
-    description="This endpoint scrapes all the links from a given website (in format \"https://example.com/\") and returns them in a dictionary format where the key is the link address and the value is the link text."
+    "/valid-links",
     )
 def get_valid_links(site: str  = Query(..., description="The URL of the site to scrape in format \"https://example.com/\"")):
     data = valid_links(site)
